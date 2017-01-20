685096824535e24b5f6032ae0cff52aaeb48446e
hansiu <unchallenged.yo@gmail.com>
2017-01-20 18:49:46 +0100

    przeszukane ligandy grupami

--- scripts/obabel.py
+++ scripts/obabel.py
@@ -1,7 +1,17 @@
-import os
+import os,sys
 
-os.system('sudo obabel ../inputfiles/ligands.sdf -ofs')
+os.system('sudo obabel obabel -isdf ../inputfiles/ligands.sdf -osmi -O../inputfiles/ligands.smi')
 
+grupy_funkcyjne = open('../inputfiles/fragments4.txt','r')
+
+i=1
+
+for line in grupy_funkcyjne:
+    grupa = line[:-1]
+    #grupa = "*".join(line[:-1].split("[*]"))
+    print(grupa)
+    os.system("obgrep '"+grupa+"' ../inputfiles/ligands.smi > ../groups/output"+str(i)+".smi")
+    i+=1
 
 
 
