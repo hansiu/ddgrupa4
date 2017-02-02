c8c1a2dc865afb2fc61adc87021a67325dfa49fc
hansiu <unchallenged.yo@gmail.com>
2017-02-02 16:20:28 +0100

    aktualizacja

--- scripts/plotting.R
+++ scripts/plotting.R
@@ -1,6 +1,5 @@
 library(ggplot2)
 library(rCharts)
-library(gridExtra)
 library(scales)
 library(tidyr)
 
@@ -32,7 +31,27 @@ inicjalizuj <- function(angs=3){
   #znalezione jakiekolwiek otoczenia/dla ilu nie?
 }
 
-przygotuj_i_zrob_ladniusi_wykres <- function(angs=3){
+
+porownaj_wagi <- function(angs=3){
+  inicjalizuj(angs)
+  
+  for(i in 1:20){
+    dane <- data.frame(aa[aa$grupa==i,2:21])
+    rownames(dane) <- c("aa")
+    dane["min_aa",] <- min_aa[min_aa$grupa==i,2:21]
+    dane["div_aa",] <- div_aa[div_aa$grupa==i,2:21]
+    dane["aaprobs",] <- t(aaprobs[aaprobs$V1==paste0("grupa",toString(1)),3])
+    dane <- as.matrix(t(dane))
+    #barplot(dane, beside=TRUE, col = c("blue","red","green"))
+    barplot(rescale(dane[,"aa"]), col="blue",main = paste0("aa grupa",toString(i)))
+    barplot(rescale(dane[,"min_aa"]), col="red",main = paste0("min_aa grupa",toString(i)))
+    barplot(rescale(dane[,"div_aa"]), col="green",main = paste0("div_aa grupa",toString(i)))
+    barplot(rescale(dane[,"aaprobs"]), col="yellow",main = paste0("aaprobs grupa",toString(i)))
+  }
+  #horiz = True?
+}
+
+przygotuj_i_zrob_ladniusie_wykresy <- function(angs=3){
   divaa <- data.frame(div_aa)
   divion <- data.frame(div_ion)
   for(i in 1:20){
@@ -43,17 +62,30 @@ przygotuj_i_zrob_ladniusi_wykres <- function(angs=3){
     
   
   }
-  divaa <- gather(divaa, 'aminokwas','wartosc', -grupa)
-  wszystkieaa <- nPlot(wartosc ~ aminokwas, group = 'grupa', data = divaa, type = 'multiBarChart')
-  wszystkieaa$save(paste0(toString(angs),'_wszystkieaa.html'))
+  #trzeba robic gather po roznych grupach, wylaczajac nulle i robic osobne wykresiki
   
-  divion <- gather(divion, 'heteroatom','wartosc', -grupa)
-  wszystkieion <- nPlot(wartosc ~ heteroatom, group = 'grupa', data = divion, type = 'multiBarChart')
-  wszystkieion$save(paste0(toString(angs),'_wszystkieion.html'))
+  #wszystkie
+  wykresl_wszystkie(divaa,divion,angs)
   
   #prwdp trzebaby dla osobnych grup
   #prawdopodobienstwa <- nPlot(V3 ~ V2, group = 'grupa', data = aaprobs, type = 'pieChart')
   
+}
+
+wykresl_wszystkie <- function(divaa,divion,angs=3){
+  divaa <- gather(divaa, 'aminokwas','wartosc', -grupa)
+  wszystkieaa <- nPlot(wartosc ~ aminokwas, group = 'grupa', data = divaa, type = 'multiBarChart')
+  wszystkieaa$xAxis( axisLabel = "aminoacid")
+  wszystkieaa$templates$script <- "http://timelyportfolio.github.io/rCharts_nvd3_templates/chartWithTitle.html"
+  wszystkieaa$set(title = "Aminoacid occurence in ligand environment")
+  wszystkieaa$save(paste0(toString(angs),'_wszystkieaa.html'))
+  
+  divion <- gather(divion, 'heteroatom','wartosc', -grupa)
+  wszystkieion <- nPlot(wartosc ~ heteroatom, group = 'grupa', data = divion, type = 'multiBarChart')
+  wszystkieion$xAxis( axisLabel = "heteroatom")
+  wszystkieaa$templates$script <- "http://timelyportfolio.github.io/rCharts_nvd3_templates/chartWithTitle.html"
+  wszystkieaa$set(title = "Heteroatom occurence in ligand environment")
+  wszystkieion$save(paste0(toString(angs),'_wszystkieion.html'))
 }
 
 
@@ -61,8 +93,8 @@ przygotuj_i_zrob_ladniusi_wykres <- function(angs=3){
 
 
 inicjalizuj()
-przygotuj_i_zrob_ladniusi_wykres()
+przygotuj_i_zrob_ladniusie_wykresy()
 inicjalizuj(5)
-przygotuj_i_zrob_ladniusi_wykres(5)
+przygotuj_i_zrob_ladniusie_wykresy(5)
 #porownaj_wagi()
 #porownaj_wagi(5)
